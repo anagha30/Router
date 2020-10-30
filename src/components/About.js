9a1c4c14ba45229abcf6c7a76604fea1397bb745
anagha30 <anaghanandanwar16@gmail.com>
2020-10-30 05:11:45 -0500

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div className="Aboutstyle">
+            <h2>Welcome to About us Pase</h2>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contact() {
+    return (
+        <div className="Aboutstyle">
+            <h2>Welcome to Contact us Page</h2>
+        </div>
+    )
+}
+
+export default Contact
